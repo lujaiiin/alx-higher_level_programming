2ce84901d38252bf2cbc09ebabf98857a041ca81
lujain <lujainm.com>
2023-10-03 07:46:06 -0700

    pythonyon

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+import sys
 import random
 number = random.randint(-10000, 10000)
 last = str(number)
 last = last[len(last) - 1]
 n = int(last)
+if number >= 10000 or number < -10000:
+    sys.stderr.write("TypeError\n")
+    exit(1)
 if number < 0:
     last = '-' + last
     n = n * -1
