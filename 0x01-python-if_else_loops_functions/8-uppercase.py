69cd4a46a1ee9e08767def17ac6b8f043c4c084e
lujain <lujainm.com>
2023-10-03 10:11:52 -0700

    pythonyon

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
-        l = ord(i)
-        if l <= 122 and l >= 97:
-            l = l - 32
-        print("{}".format(chr(l)), end='')
+        lent = ord(i)
+        if lent <= 122 and lent >= 97:
+            lent = lent - 32
+        print("{}".format(chr(lent)), end='')
     print()
