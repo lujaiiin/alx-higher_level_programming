7c5788e802dec5bc3e4f6490fee3d49d203321ad
lujain <lujainm.com>
2023-10-03 07:27:54 -0700

    pythonyon

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -13,5 +13,3 @@ elif n == 0:
     print(f"Last digit of {number} is {last} and is 0")
 elif n <= 5:
     print(f"Last digit of {number} is {last} and is less than 6 and not 0")
-else:
-    print("typeerror")
