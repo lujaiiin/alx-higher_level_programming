798f5fffd8b2409ec5ab63c5cfac96591b831d04
lujain <lujainm.com>
2023-10-05 09:17:33 -0700

    python is cool

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ import sys
 if __name__ == "__main__":
     argl = len(sys.argv)
     if argl == 1:
-        print("{} arguments.".format(argl - 1))
+        print("0 arguments.")
     else:
         print("{} arguments:".format(argl - 1))
         for i in range(1, argl):
