9445d822ef280324dc6b549c91332e9a7ff9af00
lujain <lujainm.com>
2023-10-05 06:06:22 -0700

    python is cool

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import calculator_1 as ka
+from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
     a = 10
     b = 5
-    print("{} + {} = {}".format(a, b, ka.add(a, b)))
-    print("{} - {} = {}".format(a, b, ka.sub(a, b)))
-    print("{} * {} = {}".format(a, b, ka.mul(a, b)))
-    print("{} / {} = {}".format(a, b, ka.div(a,b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a,b)))
