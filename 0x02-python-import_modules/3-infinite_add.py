5cf751307bbb44eba7a9ab5f20f100144154f220
lujain <lujainm.com>
2023-10-05 10:58:02 -0700

    python is cool

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
+import sys
 if __name__ == "__main__":
-    import sys
     argl = len(sys.argv)
     res = 0
-    if argl == 1:
-        print(0)
-    else:
+    if argl != 1:
         for i in range(1, argl):
             res = res +  int(sys.argv[i])
-        print(res)
+    print("{}".format(res))
