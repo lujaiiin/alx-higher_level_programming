d1b6c65ea15ce8988a59b60550b2b67bafdaeec7
lujain <lujainm.com>
2023-10-05 09:42:17 -0700

    python is cool

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    al = len(sys.argv)
+    alli = 0
+    if al == 1:
+        print(0)
+    else:
+        for i in range(1, al):
+            alli = int(alli) +  int(sys.argv[i])
+        print("{}".format(alli))
