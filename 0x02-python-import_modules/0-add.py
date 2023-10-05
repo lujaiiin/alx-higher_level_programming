1fc6058c5fa573e31915229b87c0d8ecc71e88b2
lujain <lujainm.com>
2023-10-05 04:51:59 -0700

    python is cool

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,4 +3,5 @@ import add_0 as add
 if __name__ == "__main__":
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add.add(a, b)))
+    re = add.add(a,b)
+    print("{} + {} = {}".format(a, b, re))
