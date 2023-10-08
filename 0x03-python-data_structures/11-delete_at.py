6be9cd123d8dee3acc4a8fd446f293ac04654881
lujain <lujainm.com>
2023-10-08 12:58:55 -0700

    python so cool

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx >= len(my_list):
+    if (idx < 0 or idx >= len(my_list)):
             return my_list
-    del my_list[idx]
-    _list = my_list
-    return _list
+    else:
+        del my_list[idx]
+        lists = my_list
+        return lists
