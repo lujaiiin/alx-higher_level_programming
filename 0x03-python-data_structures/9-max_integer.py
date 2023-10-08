31efb38b13995db1335ab5dbf25b8440a89b4c70
lujain <lujainm.com>
2023-10-08 11:52:42 -0700

    python so cool

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list:
+        sume = 0
+        for i in my_list:
+            if i > sume:
+                sume = i
+    else:
+        sume = None
+    return sume
