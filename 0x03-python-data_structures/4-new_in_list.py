72a0faa3742da283ebeff97bc8727043279069d8
lujain <lujainm.com>
2023-10-08 06:14:39 -0700

    python so cool

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,11 +2,11 @@
 def new_in_list(my_list, idx, element):
     if my_list:
         if idx < 0:
-            new_list = my_list
+            new_list = my_list.copy()
             return new_list
         if idx >= len(my_list):
-            new_list = my_list
+            new_list = my_list.copy()
             return new_list
-        new_list = my_list
+        new_list = my_list.copy()
         new_list[idx] = element
         return new_list
