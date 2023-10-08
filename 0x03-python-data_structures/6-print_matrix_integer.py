9e9b52dc5ce23a2e34d9ae62a41b6ab211f4394d
lujain <lujainm.com>
2023-10-08 11:24:09 -0700

    python so cool

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,7 +6,7 @@ def print_matrix_integer(matrix=[[]]):
         for j in i:
             l = l + 1
             if l == len(i):
-                print("{:d}".format(j), end="")
+                print("{:d}".format(j), end='')
             else:
-                print("{:d}".format(j), end=" ")
+                print("{:d}".format(j), end=' ')
         print()
