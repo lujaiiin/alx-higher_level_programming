11cdb27f6062bb765ec298f9b31389c20e0e27f9
lujain <lujainm.com>
2023-10-09 12:32:24 -0700

    python so cool

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,22 +1,25 @@
 #include <Python.h>
-#include <object.h>
-#include <listobject.h>
-/**
- * print_python_list_info - function
- * @p: value
- */
 
+/**
+ * print_python_list_info - Prints
+ * @p: A PyObject list.
+ */
 void print_python_list_info(PyObject *p)
 {
-	long int len = PyList_Size(p);
-	int i = 0;
-	PyListObject *kai = (PyListObject *)p;
+	int size, alloc, i;
+	PyObject *obj;
 
-	printf("[*] Size of the Python List = %li\n", len);
-	printf("[*] Allocated = %li\n", kai->allocated);
-	while (i < len)
+	size = Py_SIZE(p);
+	alloc = ((PyListObject *)p)->allocated;
+
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", alloc);
+
+	for (i = 0; i < size; i++)
 	{
-		print("Element %d: %s\n", i, Py_TYPE(kai->ob_item[i])->tp_name);
-		i++;
+		printf("Element %d: ", i);
+
+		obj = PyList_GetItem(p, i);
+		printf("%s\n", Py_TYPE(obj)->tp_name);
 	}
 }
