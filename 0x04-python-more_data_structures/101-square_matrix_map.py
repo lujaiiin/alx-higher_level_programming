16c62ab6e384d49dfdecc1305feb9ff31cb87121
lujain <lujainm.com>
2023-10-11 10:12:51 -0700

    set lambda map

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    li = list(map(lambda i: list(map(lambda x: x**2, i)), matrix))
-    return li
+    return list(map(lambda i: list(map(lambda x: x**2, i)), matrix))
