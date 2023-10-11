30fda2116cbee57f88d7058fb26e9550172078fd
lujain <lujainm.com>
2023-10-11 03:13:27 -0700

    set lambda map

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    li = map(lambda va : replace if va == search else va, my_list)
+    li = map(lambda va: replace if va == search else va, my_list)
     li = list(li)
     return li
