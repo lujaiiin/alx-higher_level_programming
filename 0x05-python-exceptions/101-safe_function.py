12bd2acf6d7bba2bf02bd71993816d419b9baa44
lujain <lujainm.com>
2023-10-23 13:03:39 -0700

    Errors and Exceptions

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import sys as ll
+import sys
+
 
 def safe_function(fct, *args):
     try:
         return fct(*args)
     except Exception as s:
-        ll.stderr.write("Exception: {}\n".format(s))
+        sys.stderr.write("Exception: {}\n".format(s))
         return None
