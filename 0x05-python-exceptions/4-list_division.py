172d289207bfca962aec9c220169cd1ffab34309
lujain <lujainm.com>
2023-10-23 09:28:01 -0700

    Errors and Exceptions

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -13,6 +13,7 @@ def list_division(my_list_1, my_list_2, list_length):
             res = 0
         except ZeroDivisionError:
             print("division by 0")
+            res = 0
         finally:
             list_3.append(res)
     return list_3
