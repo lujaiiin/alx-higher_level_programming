1e8157901dd917ea18f5befa755b2e6683e73001
lujain <lujainm.com>
2023-10-23 07:08:24 -0700

    Errors and Exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,9 +3,9 @@
 def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
-            print(my_list[i], end="")
+            print("{}".format(my_list[i]), end="")
         print()
         return x
-    except:
+    except IndexError:
         print()
         return i
