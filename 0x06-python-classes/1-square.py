8add2d374f2e4357b81944ef338f01fb9c26430d
lujain <lujainm.com>
2023-10-24 10:54:27 -0700

    Python - Classes and Objects

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,10 +5,10 @@
 class Square:
     """Square Class, private size."""
 
-def __init__(self, size):
-    """Initialize, The size of square.
+    def __init__(self, size):
+        """Initialize, The size of square.
 
-    Args:
-    size: size of squaew.
-    """
-    self.__size = size
+        Args:
+            size: size of squaew.
+        """
+        self.__size = size
