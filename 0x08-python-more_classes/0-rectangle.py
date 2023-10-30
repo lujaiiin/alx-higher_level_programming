50f6f3202ddd569ba00e6c7935ad72218887e3a3
lujain <lujainben@gmail.com>
2023-10-30 17:17:57 +0200

    Python OOP

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Module."""
+
+class Rectangle:
+    """class to see the Rectangle"""
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Module"""
+
+class Rectangle:
+    """Rectangle ll class"""
+    def __init__(self, width=0, height=0):
+        """intial fun"""
+        self.width = width
+        self.height = height
+
+    @property 
+    def width(self):
+        """geter fun"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter fun"""
+        if type(value) != int:
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        """get fun"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
