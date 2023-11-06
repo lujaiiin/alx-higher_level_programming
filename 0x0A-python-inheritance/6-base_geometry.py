752538a11e89c6eae9ebc831dda53ed0b35674aa
lujain <lujainm.com>
2023-11-06 09:25:32 -0800

    Inheritance in Python

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module"""
+
+
+class BaseGeometry:
+    """classy"""
+
+    def area(self):
+        """la function"""
+
+        raise Exception('area() is not implemented')
