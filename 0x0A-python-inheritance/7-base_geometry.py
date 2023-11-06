2e85f4a9d958235e4de074816fbb3bbb4660cecb
lujain <lujainm.com>
2023-11-06 09:35:01 -0800

    Inheritance in Python

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module"""
+
+
+class BaseGeometry:
+    """classy"""
+
+    def area(self):
+        """la function"""
+
+        raise Exception('area() is not implemented')
+    def integer_validator(self, name, value):
+        """int fun"""
+
+        if type(value) != int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
