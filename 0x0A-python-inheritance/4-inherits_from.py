8ecfb04b3f4ef3f6c4181a574b6ca621c44dccea
lujain <lujainm.com>
2023-11-06 09:16:05 -0800

    Inheritance in Python

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,6 +4,6 @@
 def inherits_from(obj, a_class):
     """inhert fun"""
 
-    if dir(obj) == dir(a_class) and isinstance(obj, a_class) and type(obj) is not a_class:
+    if type(obj) is not a_class:
         return True
     return False
