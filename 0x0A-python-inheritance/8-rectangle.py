29b7e99c56427be1e6c08e5ed82b4d7fcab7813c
lujain <lujainm.com>
2023-11-06 10:11:31 -0800

    Inheritance in Python

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Mudules"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """rec class inhert a base class"""
+
+    def __init__(self, width, height):
+        """init function"""
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
