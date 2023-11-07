5df0929f157304359ba3b2f736645054ef60b92d
lujain <lujainm.com>
2023-11-07 11:01:04 -0800

    files

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 def read_file(filename=""):
     """function read file"""
 
-    with open(filename, 'r') as l:
+    with open(filename, 'r', encoding="utf-8") as l:
         print(l.read(), end='')
