0505d8054429c380c00ed99f27de86005b2b1bc4
lujain <lujainm.com>
2023-11-07 13:44:43 -0800

    files

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,16 +1,42 @@
 #!/usr/bin/python3
-import random
-import sys
-from time import sleep
-import datetime
+"""Modules"""
 
 
-for _ in range(10000):
-    sleep(random.random())
-    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
-        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
-        datetime.datetime.now(),
-        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
-        random.randint(1, 1024)
-    ))
-    sys.stdout.flush()
+def print_stats(size, status_codes):
+    """function"""
+    print("File size: {}".format(size))
+    for i in sorted(status_codes):
+        print("{}: {}".format(i, status_codes[i]))
+
+if __name__ == "__main__":
+    import sys
+
+    size = 0
+    status_codes = {}
+    va = ['200', '301', '400', '401', '403', '404', '405', '500']
+    c = 0
+
+    try:
+        for lin in sys.stdin:
+            if c == 10:
+                print_stats(size, status_codes)
+                c = 1
+            else:
+                c += 1
+            lin = lin.split()
+            try:
+                size += int(lin[-1])
+            except (IndexError, ValueError):
+                pass
+            try:
+                if lin[-2] in va:
+                    if status_codes.get(lin[-2], -1) == -1:
+                        status_codes[lin[-2]] = 1
+                    else:
+                        status_codes[lin[-2]] += 1
+            except IndexError:
+                pass
+        print_stats(size, status_codes)
+    except KeyboardInterrupt:
+        print_stats(size, status_codes)
+        raise
