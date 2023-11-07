1cd07079165579b82c8eddbb5bcdee42607be400
lujain <lujainm.com>
2023-11-07 11:58:50 -0800

    files

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """functions"""
-    with open(filename, "w+") as l:
-        l.write(json.dumps(my_obj))
+    with open(filename, "w+") as fil:
+        fil.write(json.dumps(my_obj))
