227a383324d11acb800621a3aa93879193a47574
lujain <lujainm.com>
2024-01-08 08:14:44 -0800

    java script

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@ if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
   let i;
-  let x = Number(process.argv[2]);
+  const x = Number(process.argv[2]);
   for (i = 0; i < x; i++) {
     console.log('X'.repeat(x));
   }
