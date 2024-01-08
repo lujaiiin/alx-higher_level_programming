e74ac3e1e6e92a23fe2f1f2bf7345db483f67e29
lujain <lujainm.com>
2024-01-08 09:13:26 -0800

    java script

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 function factor (num) {
- let i;
- if (num === 0 || num === 1 || isNaN(num)) {
-   return 1;
- } else if (num < 0) {
-   return -1;
- }
- for (i = num - 1; i >= 1; i--) {
-   num = num * i;
- }
- return num;
+  let i;
+  if (num === 0 || num === 1 || isNaN(num)) {
+    return (1);
+  } else if (num < 0) {
+    return (-1);
+  }
+  for (i = num - 1; i >= 1; i--) {
+    num = num * i;
+  }
+  return num;
 }
 console.log(factor(Number(process.argv[2])));
