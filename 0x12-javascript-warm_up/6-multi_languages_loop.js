cc54c5f3d5ec5c8de02fe8304e922a54eb5cb22d
lujain <lujainm.com>
2024-01-08 08:46:29 -0800

    java script

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-for (const i of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
-  console.log(i);
+for (const li of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(li);
 }
