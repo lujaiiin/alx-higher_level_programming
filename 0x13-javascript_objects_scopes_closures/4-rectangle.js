32bc4d3aadef8321e176aaa5a6f03757d1a41b24
lujain <lujainm.com>
2024-01-09 13:36:13 -0800

    JavaScript

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i;
+    let j;
+    let x;
+    for (i = 0; i < this.height; i++) {
+      x = '';
+      for (j = 0; j < this.width; j++) {
+        x += 'X';
+      }
+      console.log(x);
+    }
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+  
+  rotate () {
+    const aux = this.width;
+    this.width = this.height;
+    this.height = aux;
+  }
+}
+
+module.exports = Rectangle;
