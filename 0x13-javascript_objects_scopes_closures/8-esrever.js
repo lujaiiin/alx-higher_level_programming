27ce836ec1885f17691e121372177c7e5bedd89c
lujain <lujainm.com>
2024-01-09 14:18:01 -0800

    JavaScript

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 exports.esrever = function (list) {
   let l = list.length - 1;
   let j = 0;
-  for (l; (len - j) > 0; l--) {
+  for (; (l - j) > 0; l--) {
     const tmp = list[l];
     list[l] = list[j];
     list[j] = tmp;
