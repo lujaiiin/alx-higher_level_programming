da9e6fae2b59ff577ed1ffaf0f0b16c8e1c0eeab
lujain <lujainm.com>
2024-01-09 14:01:49 -0800

    JavaScript

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const SquareP = require('./5-square');
+
+class Square extends SquareP {
+  charPrint (c) {
+    let i;
+    let j;
+    let x;
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (i = 0; i < this.height; i++) {
+        x = '';
+      for (j = 0; j < this.width; j++) {
+        x += c;
+      }
+      console.log(x);
+    }
+  }
+}
+
+module.exports = Square;
